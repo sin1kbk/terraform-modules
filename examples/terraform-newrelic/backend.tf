a5cab6b8495260f75aa626088bf08257cd9e9b9b
shinichi_kuboki <kuboki.business@gmail.com>
2023-03-28 14:25:22 +0900

    t

--- examples/terraform-newrelic/backend.tf
+++ examples/terraform-newrelic/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   #backend "local" { path = ".tfstate" }
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "kubokkey"
 
     workspaces {
