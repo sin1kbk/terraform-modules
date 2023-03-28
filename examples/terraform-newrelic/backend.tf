5654632a37e170c62c487813f84e9fcb1ffa08f7
shinichi_kuboki <kuboki.business@gmail.com>
2023-03-28 14:42:27 +0900

    t

--- examples/terraform-newrelic/backend.tf
+++ examples/terraform-newrelic/backend.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.56.0"
+      version = "3.74.0"
     }
     newrelic = {
       source  = "newrelic/newrelic"
