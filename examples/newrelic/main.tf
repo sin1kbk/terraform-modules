f6ab42c94c0d458d37d72d92b7e73110304b895d
shinichi_kuboki <kuboki.business@gmail.com>
2023-03-29 23:28:41 +0900

    feat: add newrelic

---
+++ examples/newrelic/main.tf
@@ -0,0 +1,6 @@
+module "newrelic" {
+    source = "github.com/kubokkey/terraform-newrelic"
+    newrelic_account_id = var.NEWRELIC_ACCOUNT_ID
+    newrelic_aws_account_id = var.NEWRELIC_AWS_ACCOUNT_ID
+    newrelic_api_key = var.NEWRELIC_API_KEY
+}
---
+++ examples/newrelic/variable.tf
@@ -0,0 +1,3 @@
+variable "NEWRELIC_ACCOUNT_ID" { type = string }
+variable "NEWRELIC_API_KEY" { type = string }
+variable "NEWRELIC_AWS_ACCOUNT_ID" { type = string }
