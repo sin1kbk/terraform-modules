e16aae01a70ae2a224ea9600e92ed7eba6760452
shinichi_kuboki <kuboki.business@gmail.com>
2023-03-30 01:41:04 +0900

    chore

--- examples/newrelic/provider.tf
+++ examples/newrelic/provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  #backend "local" { path = ".tfstate" }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "3.15.0"
+    }
+  }
+}
+
 provider "newrelic" {
   account_id = var.NEWRELIC_ACCOUNT_ID
   api_key    = var.NEWRELIC_API_KEY
